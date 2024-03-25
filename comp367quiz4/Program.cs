f477189f282ebfdc3c6eefa1a5721d3be2f63106
Marcus <123912646+marcuschui2022@users.noreply.github.com>
2024-03-25 14:52:13 -0400

    Update Program.cs
    resolves #2

--- comp367quiz4/Program.cs
+++ comp367quiz4/Program.cs
@@ -4,6 +4,7 @@ namespace comp367quiz4
     {
         public static void Main(string[] args)
         {
+            // migrate local Database to the cloud
             // enable web app to access data stored in local database, and display all data on the web page
             var builder = WebApplication.CreateBuilder(args);
 
